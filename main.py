22f3274f3672b5b4be70ea43d347e0b438298c40
Lucas Farias <lucasf1@gmail.com>
2023-09-20 21:40:11 -0300

    Rotas para usuários, admins e autenticação

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from database import engine
 from fastapi import FastAPI
-from routers import auth, todos
+from routers import auth, todos, admin, users
 import models
 
 app = FastAPI()
@@ -9,3 +9,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
